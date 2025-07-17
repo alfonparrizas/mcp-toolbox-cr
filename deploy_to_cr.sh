d67794adbacee1503742feec0c685ebb27f67974
alfonparrizas <alfonparrizas@gmail.com>
2025-07-17 09:13:00 +0000

    Change location to Madrid

--- deploy_to_cr.sh
+++ deploy_to_cr.sh
@@ -1,4 +1,6 @@
-gcloud secrets create tools --data-file=tools.yaml
+#!/bin/bash
+
+#gcloud secrets create tools --data-file=tools.yaml
 gcloud secrets versions add tools --data-file=tools.yaml
 
 export IMAGE=us-central1-docker.pkg.dev/database-toolbox/toolbox/toolbox:latest
@@ -6,7 +8,7 @@ export IMAGE=us-central1-docker.pkg.dev/database-toolbox/toolbox/toolbox:latest
 gcloud run deploy toolbox \
     --image $IMAGE \
     --service-account toolbox-identity \
-    --region us-central1 \
+    --region europe-southwest1 \
     --set-secrets "/app/tools.yaml=tools:latest" \
     --args="--tools-file=/app/tools.yaml","--address=0.0.0.0","--port=8080" \
     --allow-unauthenticated
